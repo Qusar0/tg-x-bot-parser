64b46d65edfab5489062b9e310b3c1d13dc1b153
Nafan4x <tim351455@gmail.com>
2025-10-03 17:24:23 +0300

    работа над парсером по x

--- app/xscrapper/__init__.py
+++ app/xscrapper/__init__.py
@@ -16,7 +16,12 @@ from app.enums import WordType
 from app.settings import settings
 from app.bot.Manager import BotManager
 from app.database.models.Word import Word
-from app.helpers import preprocess_text, is_word_match, is_duplicate, get_fetched_post_ids
+from app.helpers import (
+    preprocess_text,
+    is_word_match,
+    is_duplicate,
+    get_fetched_post_ids,
+)
 from app.queue import queue
 
 with open("x_cookies.txt") as file:
@@ -45,8 +50,7 @@ class XScrapper:
             #     "password": "6iogl9"
             # }
         )
-        self.context = await self.browser.new_context(
-        )
+        self.context = await self.browser.new_context()
 
     async def _load_cookie(self):
         self.is_load_cookie = True
@@ -76,7 +80,7 @@ class XScrapper:
                     await self.input_search_keywords(keyword.title, is_first_search)
 
                     await self.parse_posts(keyword.central_chat_id, stopwords, keyword)
-                    #self.load_more_posts()
+                    # self.load_more_posts()
 
                 # except ElementNotInteractableException:
                 #     logger.warning('На странице не найдено больше записей.')
@@ -92,6 +96,7 @@ class XScrapper:
             traceback.print_exc()
 
     async def parse_posts(self, chat_id: int, stopwords: list[Word], keyword: Word):
+        """Поиск постов, сбор информации по определенному ключ-слову"""
         FETCHED_CARDS = await get_fetched_post_ids()
         while True:
             exit_loop = False
@@ -102,76 +107,84 @@ class XScrapper:
             soup = BeautifulSoup(html_content, "lxml")
 
             cards = soup.select("article[data-testid='tweet']")
-            print(len(cards))
             for card in cards:
                 time_tag = card.find("time")
                 if time_tag:
                     datetime_str = time_tag.get("datetime")
-                    tweet_time = datetime.fromisoformat(datetime_str.replace("Z", "+00:00"))  # UTC
+                    tweet_time = datetime.fromisoformat(
+                        datetime_str.replace("Z", "+00:00")
+                    )
                     now = datetime.now(timezone.utc)
 
-                    # Разница во времени
                     delta = now - tweet_time
 
-                    # Проверяем, что не старше 24 часов
                     if delta > timedelta(days=1):
-                        exit_loop = True  # ставим флаг выхода
+                        # Точка выхода, если возраст поста > 24h
+                        exit_loop = True
                         break
+                # Парс текста
+                tweet_div = card.find("div", {"data-testid": "tweetText"})
 
+                # if tweet_div:
+                #     tweet_text = tweet_div.get_text(separator=" ", strip=True)
+                #     print(tweet_text)
+                tweet_div = str(tweet_div)
 
-                tweet_div = card.find('div', {'data-testid': 'tweetText'})
-                if tweet_div:
-                    tweet_text = tweet_div.get_text(separator=' ', strip=True)
-                    print(tweet_text)
-
+                # Парс медиа
                 photo_divs = card.find_all("div", {"data-testid": "tweetPhoto"})
                 imgs = []
                 for div in photo_divs:
-                    img = div.find("img")  # ищем тег <img> внутри div
+                    img = div.find("img")
                     if img and img.get("src"):
                         imgs.append(img["src"])
-                print(imgs)
 
+                # Парс идентификатора
                 a_tag = card.find("a", href=lambda x: x and "/status/" in x)
                 if a_tag:
-                    id = a_tag.get("href").split('/')[-1]
-                    print(id)
+                    id = a_tag.get("href").split("/")[-1]
+
                 if id in FETCHED_CARDS:
                     continue
-
+                print(id)
                 FETCHED_CARDS.append(id)
 
+                if is_word_match(tweet_div, stopwords):
+                    logger.info(f"Нашли стоп-слова в сообщении: {id}")
+                    continue
+
+                if await is_duplicate(id, tweet_div):
+                    logger.info(f"Сообщение дубликат: {id}")
+                    return
+
+                processed_text = preprocess_text(tweet_div, keyword)
+                print(processed_text)
+                if len(imgs) > 1:
+                    asyncio.create_task(
+                        queue.call(
+                            (BotManager.send_media_group, chat_id, imgs, processed_text)
+                        )
+                    )
+                elif len(imgs) == 1:
+                    asyncio.create_task(
+                        queue.call(
+                            (BotManager.send_photo, chat_id, img[0], processed_text)
+                        )
+                    )
+                else:
+                    asyncio.create_task(
+                        queue.call((BotManager.send_message, chat_id, processed_text))
+                    )
             if exit_loop:
+                logger.info("Скролим страницу и ищем новые посты")
                 break
 
-            await self.page.evaluate("""
+            logger.info("Скролим страницу и ищем новые посты")
+            await self.page.evaluate(
+                """
                 window.scrollBy(0, window.innerHeight);
-            """)
+            """
+            )
             await self.page.wait_for_timeout(1000)
-
-                # if id in FETCHED_CARDS:
-                #     continue
-
-                # FETCHED_CARDS.append(id)
-
-                # if is_word_match(text, stopwords):
-                #     logger.info(f"Нашли стоп-слова в сообщении: {id}")
-                #     continue
-
-                # if await is_duplicate(id, text):
-                #     logger.info(f"Сообщение дубликат: {id}")
-                #     return
-
-                # processed_text = preprocess_text(text, keyword)
-
-                # if carousel_img:
-                #     asyncio.create_task(queue.call((BotManager.send_media_group, chat_id, imgs, processed_text)))
-                # elif single_img:
-                #     single_img = single_img.get("src")
-                #     asyncio.create_task(queue.call((BotManager.send_photo, chat_id, single_img, processed_text)))
-                # else:
-                #     asyncio.create_task(queue.call((BotManager.send_message, chat_id, processed_text)))
-
 
     async def load_search_page(self):
         """Грузим поисковую страницу"""
@@ -193,24 +206,10 @@ class XScrapper:
         await self.page.press(input_selector, "Enter")
         time.sleep(1)
         if is_first_search:
-            latest_button = '//nav/div/div[2]/div/div[2]/a'
+            latest_button = "//nav/div/div[2]/div/div[2]/a"
             await self.page.locator(latest_button).click()
 
         logger.debug(f"Делаем запрос с ключами: {keyword_request}")
-
-    # def load_more_posts(self):
-    #     """Нажимаем кнопку загрузить еще"""
-    #     logger.info("Открываем больше постов")
-    #     try:
-    #         button = self.driver.find_element(By.XPATH, '//*[@id="sticky-center-column"]/div[3]/div[2]/button')
-    #         click_count = settings.get_scrapper_more_posts_clicks_count()
-
-    #         for _ in range(click_count):
-    #             button.click()
-    #             time.sleep(2)
-
-    #     except NoSuchElementException:
-    #         logger.warning("Не нашли кнопки для загрузки дополнительных постов")
 
     async def start(self):
         await self._load_driver()
@@ -226,7 +225,9 @@ class XScrapper:
                 sleep_sec = settings.get_scrapper_page_sleep_sec()
                 sleep_sec = random.randint(sleep_sec, sleep_sec + 60)
                 logger.info(f"Спим: {sleep_sec} сек.")
-                await self.page.wait_for_timeout(29999000)  # пауза 2 секунды (в миллисекундах)
+                await self.page.wait_for_timeout(
+                    29999000
+                )  # пауза 2 секунды (в миллисекундах)
 
         finally:
             await self.browser.close()
